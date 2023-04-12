066cd7909443d0d5225303ee9e843906ec04ac57
Ethan-mo <ethan.mo@goodmonit.com>
2023-04-12 19:16:29 +0900

    [Setting] 첫 ViewController 설정

---
+++ InstagramClone/Controller/MainTabController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabController.swift
+//  InstagramClone
+//
+//  Created by 모상현 on 2023/04/12.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
